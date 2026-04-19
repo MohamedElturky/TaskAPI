d819dcec962971fb426d4171672a215c45d0a487
MohamedElturky <153658210+MohamedElturky@users.noreply.github.com>
2026-04-19 19:37:52 +0200

    task

--- TaskApi/src/TaskApi.Domain/obj/Debug/net8.0/TaskApi.Domain.AssemblyInfo.cs
+++ TaskApi/src/TaskApi.Domain/obj/Debug/net8.0/TaskApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1ad8da174de7319960e414731eac33dbcf9619")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
